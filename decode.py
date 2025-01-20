ea62d7605b7911c15ed8172f20e08429e045fb4b
Dipesh Acharya <dipeshacharya87@gmail.com>
2025-01-20 23:46:51 +0545

    added zip encoding and decoding

--- decode.py
+++ decode.py
@@ -1,13 +1,21 @@
 from PIL import Image
 import numpy as np
+import zipfile
+import io
 
-def binary_to_string(binary):
-    byte_array = np.packbits(binary)
-    output = byte_array.tobytes().decode()
-    return output
+def binary_to_zip(binary):
+    byte_data = bytes(np.packbits(binary))
+    zip_data = io.BytesIO(byte_data)
+    try:
+        with zipfile.ZipFile(zip_data, 'r') as zip_ref:
+            zip_ref.extractall('extracted_files')
+        return "Zip file extracted successfully"
+    except zipfile.BadZipFile:
+        return "File is not a zip file"
 
-image1 = Image.open('modified_image (2).png')
+image1 = Image.open('modified_image.png')
 arr = np.array(image1)
+print(arr.size)
 height, width, _ = arr.shape
 
 binary = []
@@ -15,16 +23,16 @@ count = 0
 flag = 0
 for y in range(height):
     for x in range(width):
-            if flag < 7:
-                pixel_value =  arr[y, x , 2]
-                pixel_value = bin(pixel_value)
-                last_bit = int(pixel_value[-1])
-                if last_bit == 1:
-                     flag += 1
-                else:
-                     flag = 0
-                binary.append(last_bit)
-                count +=1
-binary = (np.array(binary[:-8] , dtype=np.uint8))
-print('Your hidden message is :->',binary_to_string(binary))
-            
+        if flag < 83:
+            pixel_value = arr[y, x, 2]
+            last_bit = pixel_value & 1
+            binary.append(last_bit)
+            if last_bit == 1:
+                flag += 1
+            else:
+                flag = 0
+            count += 1
+
+binary = np.array(binary[:-84], dtype=np.uint8)
+print(binary.size)
+print('Your hidden message is :->', binary_to_zip(binary))
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import numpy as np
+import zipfile
+import io
 
-image1 = Image.open('disabled.png')
+image1 = Image.open('test.png')
 
 arr = np.array(image1)
-print(arr.shape)
+print(arr.size)
 
 height, width, _ = arr.shape
 print(arr[0,0,2])
@@ -14,11 +16,29 @@ def string_to_binary(text):
     binary_array = np.unpackbits(binary_array)
     return binary_array
 
+def file_to_zip_binary(file_path):
+    file_name = file_path.split('/')[-1]
 
+    buffer = io.BytesIO()
+    # zip file in the buffer
+    with zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        zip_file.write(file_path, arcname=file_name)
+    
+    # Save the zip file to the directory
+    # with open('file.zip', 'wb') as f:
+    #     f.write(buffer.getvalue())
+    
+    zip_binary = np.frombuffer(buffer.getvalue(), dtype=np.uint8)
+    
+    # binary data to bits
+    binary_array = np.unpackbits(zip_binary)
+    
+    return binary_array
 
 
 def embedd_message(text):
-    end_bits = np.array([0,1,1,1,1,1,1,1] , dtype=np.uint8)
+    #adding 83 1's to the end of the message
+    end_bits = np.array([0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] , dtype=np.uint8)
     text = np.concatenate((text, end_bits))
     text_length = text.size
     i = 0
@@ -33,12 +53,39 @@ def embedd_message(text):
     new_image = Image.fromarray(arr)
     new_image.save('modified_image.png')
 
+def analyze_consecutive_bits(binary_array):
+    max_consecutive_ones = 0
+    max_consecutive_zeros = 0
+    current_ones = 0
+    current_zeros = 0
+    
+    for bit in binary_array:
+        if bit == 1:
+            current_ones += 1
+            current_zeros = 0
+        else:
+            current_zeros += 1
+            current_ones = 0
+        
+        if current_ones > max_consecutive_ones:
+            max_consecutive_ones = current_ones
+        if current_zeros > max_consecutive_zeros:
+            max_consecutive_zeros = current_zeros
+    
+    return max_consecutive_ones, max_consecutive_zeros
+
 def main():
-    message = input("What message do you want to hide?  ")
-    text = string_to_binary(message)
+    # message = input("What message do you want to hide?  ")
+    # text = string_to_binary('message')
+    # embedd_message(text)
+    # print('your message is now hidden.')
+    text = file_to_zip_binary('test.py')
+    print(text)
+    # a , b  = analyze_consecutive_bits(text)
+    # print(a)
+    # print(b)
+    print(text.size)
     embedd_message(text)
-    print('your message is now hidden.')
+    print('your file is now hidden.')
 
 main()
-
-
