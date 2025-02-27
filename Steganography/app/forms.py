ef46f3da9c43294666107809ecc5948e9995f17d
Dipesh Acharya <dipeshacharya87@gmail.com>
2025-02-27 20:53:03 +0545

    ui improvements --safari/chrome fix needed

--- Steganography/app/forms.py
+++ Steganography/app/forms.py
@@ -2,27 +2,27 @@ from django import forms
 from .models import *
 
 class EncodeMessageForm(forms.ModelForm):
-    pin = forms.CharField(max_length=4, widget=forms.PasswordInput)
+    pin = forms.CharField(max_length=4, widget=forms.PasswordInput(attrs={'value': '0000'}))
 
     class Meta:
         model = EncodeMessage
         fields = ['Message', 'pin', 'Image',]
         
 class DecodeMessageForm(forms.ModelForm):
-    pin = forms.CharField(max_length=4, widget=forms.PasswordInput)
+    pin = forms.CharField(max_length=4, widget=forms.PasswordInput(attrs={'value': '0000'}))
     class Meta:
         model = DecodeMessage
         fields = ['Image' , 'pin' ,]
         
 class EncodeFileForm(forms.ModelForm):
-    pin = forms.CharField(max_length=4, widget=forms.PasswordInput)
+    pin = forms.CharField(max_length=4, widget=forms.PasswordInput(attrs={'value': '0000'}))
     class Meta:
         model = EncodeFile
         fields = ['pin', 'Image','File',]
 
 
 class DecodeFileForm(forms.ModelForm):
-    pin = forms.CharField(max_length=4, widget=forms.PasswordInput)
+    pin = forms.CharField(max_length=4, widget=forms.PasswordInput(attrs={'value': '0000'}))
     class Meta:
         model = DecodeFile
         fields = ['Image' , 'pin' ,]
